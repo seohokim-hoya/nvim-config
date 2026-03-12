9d9b377099e340dfc3ecb4f98efadad6fe4bd848
seohokim-hoya <seoho7777.kim@gmail.com>
2026-03-12 13:29:04 +0900

    Update MarkdownPreview.nvim

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,12 +1,22 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	ft = { "markdown" },
-	cmd = { "MarkdownPreview", "MarkdownPreviewToggle", "MarkdownPreviewStop" },
-	build = "cd app && npm install",
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-	end,
-	keys = {
-		{ "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
+	{
+		"iamcco/markdown-preview.nvim",
+		ft = { "markdown" },
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+		init = function()
+			vim.g.mkdp_echo_preview_url = 1
+			vim.g.mkdp_browserfunc = "MkdpOpenWSL"
+			vim.cmd([[
+	     function! MkdpOpenWSL(url)
+	       execute 'silent !wslview ' . shellescape(a:url)
+	     endfunction
+	   ]])
+		end,
+		keys = {
+			{ "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
+		},
 	},
 }
